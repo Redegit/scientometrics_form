01fb9eb3f74e696b3d6c5009842e53a2548f8bfe
Redegit <dima070603@gmail.com>
2023-06-03 21:18:10 +0300

    Добавлена индикация ошибки с пробелами

--- src/components/InputForMulti.jsx
+++ src/components/InputForMulti.jsx
@@ -4,14 +4,23 @@ import Tooltip from "./Tooltip/Tooltip";
 export const InputForMulti = ({ register, root, name, index, errors, trigger, placeholder, constraints, tooltip }) => {
 
     const checkError = () => {
-        if (_.get(errors, `${root}[${index}].${name}`)) {
+        const err = _.get(errors, `${root}[${index}].${name}`)
+        if (err) {
+            return true
+        }
+        return false
+    }
+
+    const checkWhiteSpaces = () => {
+        const err = _.get(errors, `${root}[${index}].${name}`)
+        if (err && err.type === 'pattern') {
             return true
         }
         return false
     }
 
     return (
-        <span className={`d-inline-block my-tooltip  ${tooltip ? "input-tooltip" : ""}`} tabIndex="0" data-toggle="tooltip" title={tooltip}>
+        <span className={`d-inline-block my-tooltip ${checkWhiteSpaces() ? "form-invalid-spaces" : ''} ${tooltip ? "input-tooltip" : ""}`} tabIndex="0" data-toggle="tooltip" title={tooltip}>
             <input
                 className={`form-control ${checkError() ? "is-invalid" : ""}`}
                 {...register(`${root}[${index}].${name}`,
